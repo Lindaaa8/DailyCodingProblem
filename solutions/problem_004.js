20d5227586429012436fa704f123861b91ee611d
Linda He <heyixuan64@gmail.com>
2019-07-09 00:04:16 -0400

    faster solutions

--- solutions/problem_004.js
+++ solutions/problem_004.js
@@ -20,3 +20,22 @@ var removeNthFromEnd = function(head, n) {
 
 // step 2: Could you do this in one pass?
 
+var removeNthFromEnd = function(head, n) {
+    let new_head = new ListNode(0);
+    new_head.next = head;
+    let prev = cur = new_head;
+    let num = 0;
+    while (cur.next !== null) {
+        if (num < n) {
+            num++;
+            cur = cur.next;
+        } else {
+            cur = cur.next;
+            prev = prev.next;
+        }
+    }
+    if (prev.next !== null) {
+        prev.next = prev.next.next;
+    }
+    return new_head.next;
+}
